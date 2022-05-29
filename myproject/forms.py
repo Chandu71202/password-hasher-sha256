78e6d8c7388abaf17feef34101ee7fda475b4a5a
chandu71202 <chandu71202@gmail.com>
2022-05-29 11:26:21 +0530

    updated requirements

--- myproject/forms.py
+++ myproject/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired,Email
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
